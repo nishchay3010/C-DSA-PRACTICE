20d3dc64fc669f4a4b47d1aef283c08917a3859b
nishchay <nishchayk1230@gmail.com>
2026-09-07 18:13:55 +0530

    factorial

--- fibonacci.c
+++ fibonacci.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-void fibonacci(int n);
+void factorial(int n);
 
 int main() {
     int n;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    fibonacci(n);
+    factorial(n);
 
     return 0;
 }
-void fibonacci(int n){
+void factorial(int n){
     
-    int t1 = 0, t2 = 1, nextTerm;
-    printf("Fibonacci Series: ");
-    for (int i=1;i<=n;i++){
-        printf("%d\n", t1);
-        nextTerm = t1+t2;
-        t1=t2;
-        t2=nextTerm;
+    int fact = 1;
+    for (int i = 1;i<=n;i++){
+        fact *= i;
+        printf("Factorial of %d is: %d\n", i, fact);
     }
-
 }
