b64111fe812fcd95cf8cb9b4852730fe02db1c7c
nishchay <nishchayk1230@gmail.com>
2026-09-07 18:19:47 +0530

    fibonacci

--- factorial.c
+++ factorial.c
@@ -11,11 +11,5 @@ int main() {
     return 0;
 }
 void fibonacci(int n){
-    int t1 = 0, t2 = 1, nextTerm;
-    for (int i = 1; i <= n; ++i) {
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        printf("%d, ", nextTerm);
-    }
+    
 }
