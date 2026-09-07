e94741b561db8c390fb7ac604eefa39887178191
nishchay <nishchayk1230@gmail.com>
2026-09-07 17:21:07 +0530

    fibonacci series

---
+++ fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void fibonacci(int n);
+
+int main() {
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    fibonacci(n);
+
+    return 0;
+}
+void fibonacci(int n){
+    
+    int t1 = 0, t2 = 1, nextTerm;
+    printf("Fibonacci Series: ");
+    for (int i=1;i<=n;i++){
+        printf("%d\n", t1);
+        nextTerm = t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
+
+}
